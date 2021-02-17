96d96bec4dd6b85a9a689a31aad0863f26838708
Eva van Rooijen <evav.rooijen@live.nl>
2021-02-17 22:19:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+st.set_page_config(page_title='Climate Modeling', page_icon='earth.jpg', layout='wide', initial_sidebar_state='expanded')
 
 st.sidebar.write('Hello World!')
 
